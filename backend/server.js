e0f90c672378339cb72db9a0f1ce8157331d40f1
Markus Maelzer <markus.maelzer@gmail.com>
2018-10-08 10:17:14 +0200

    switche body-parser for express parsing, removed draft-js remnants

--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,37 @@
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const gm = require('gm');
 const _ = require('lodash');
 const sharp = require('sharp');
+const path = require('path');
 
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-app.use(bodyParser.json());
-// gotta check if neede >_>
-app.use(bodyParser.urlencoded({ extended: true }));
+const staticDirectory = path.join(__dirname, '../storyboard-creator-frontend/build');
+
+app.use(express.json({type: '*/*'}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('combined'));
 
-const staticDirectory = `${__dirname}/../storyboard-creator-frontend/build`;
 app.use(express.static(staticDirectory))
 
-var editorState;
 
+let editorState;
 app.get('/editor-state', (req, res) => {
   res.send(editorState);
 })
 
-
 io.on('connection', (socket) => {
-  // socket.emit('welcome', 'Hello WOrld');
-  // socket.on('check-editor', (data) => {
-  //   console.log('hi');
-  //   console.log(editorState);
-  //   if(editorState)
-  //     socket.emit('editor-state', editorState);
-  // })
-  //
-  // socket.on('editor-state', (editorData) => {
-  //   editorState = editorData;
-  //   socket.broadcast.emit('editor-state', editorData)
-  // })
   socket.on('sync-editor', ({operations, state}) => {
     editorState = state;
     socket.broadcast.emit('sync-editor', operations);
   })
 })
-
 
 server.listen(3001, () => {
   console.log('server is startet on port 3001');
